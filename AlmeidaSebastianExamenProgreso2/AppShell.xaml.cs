6304bd941b14b035487d1f9a74dad7b1b3c962d7
Andres Sebastian Almeida Yanez <andres.almeida.yanez@udla.edu.ec>
2024-12-02 07:15:10 -0500

    Add project files.

---
+++ AlmeidaSebastianExamenProgreso2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlmeidaSebastianExamenProgreso2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AlmeidaSebastianExamenProgreso2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AlmeidaSebastianExamenProgreso2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
