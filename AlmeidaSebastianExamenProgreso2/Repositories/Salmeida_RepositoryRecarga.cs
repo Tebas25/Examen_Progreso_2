6cddf5b9f6c1107c5ccdcb3595d76037560282d0
Andres Sebastian Almeida Yanez <andres.almeida.yanez@udla.edu.ec>
2024-12-02 07:44:54 -0500

    Creación del repositorio CRUD con los métodos Create & Get

--- AlmeidaSebastianExamenProgreso2/Repositories/Salmeida_RepositoryRecarga.cs
+++ AlmeidaSebastianExamenProgreso2/Repositories/Salmeida_RepositoryRecarga.cs
@@ -1,18 +1,24 @@
 ﻿using AlmeidaSebastianExamenProgreso2.Interfaces;
 using AlmeidaSebastianExamenProgreso2.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AlmeidaSebastianExamenProgreso2.Repositories
 {
     public class Salmeida_RepositoryRecarga : ISalmeidaRecarga
     {
+        private string _fileName = Path.Combine(FileSystem.AppDataDirectory, "SebastianAlmeida.txt");
         public bool CreateRecarga(SalmeidaRecarga recarga)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string json_data = JsonConvert.SerializeObject(recarga);
+                File.WriteAllText(_fileName, json_data);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public bool DeleteRecarga(string numero)
@@ -22,7 +28,13 @@ namespace AlmeidaSebastianExamenProgreso2.Repositories
 
         public SalmeidaRecarga GetRecarga(string numero)
         {
-            throw new NotImplementedException();
+            SalmeidaRecarga recarga = new SalmeidaRecarga();
+            if (File.Exists(_fileName))
+            {
+                string josn_data = File.ReadAllText(_fileName);
+                recarga = JsonConvert.DeserializeObject<SalmeidaRecarga>(josn_data);
+            }
+            return recarga;
         }
 
         public bool UpdateRecarga(SalmeidaRecarga recarga)
